7a1ecb50aa1d06cf95d1ef4ea25699bd814d47f8
tommaso auerbach <tsa302@nyu.edu>
2019-06-17 18:51:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,6 +30,6 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array, "tide")
-  array.insert("tide")
+def using_insert(array, tide)
+  array.insert(1,tide)
 end
