a72003068a875ecc5e0448a620132d5da6250f13
tommaso auerbach <tsa302@nyu.edu>
2019-06-17 19:01:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,11 +39,11 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
-
+  array.flatten
 end
 
-def using_delete(array)
-
+def using_delete(array, string)
+  array.delete(string)
 end
 
 def using_delete_at
