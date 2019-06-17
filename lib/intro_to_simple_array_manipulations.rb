ee67ba423ef80a88cdaf3cfe64a2658f838c5ed0
tommaso auerbach <tsa302@nyu.edu>
2019-06-17 18:26:49 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(array, string)
+  array = ["1", "2", "3"]
+  string = "Niger"
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array = ["1", "2", "3", "4", "5"]
+  string = "Brooklyn Heights"
+  array.unshift(string)
+end
+
+def using_pop(great_hits_of_the_nineties)
+  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  great_hits_of_the_nineties.pop
+end
