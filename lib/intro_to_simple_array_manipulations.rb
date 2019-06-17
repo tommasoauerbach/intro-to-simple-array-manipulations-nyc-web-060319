09feda4c6e6d04a7cc77d42f23aac3c4b396ca44
tommaso auerbach <tsa302@nyu.edu>
2019-06-17 18:30:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,3 +14,5 @@ def using_pop(great_hits_of_the_nineties)
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
   great_hits_of_the_nineties.pop
 end
+return great_hits_of_the_nineties
+end
